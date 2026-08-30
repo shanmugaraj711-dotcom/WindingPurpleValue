f5911c2c136dc8ad69bcd17a9388b52e05a02d86
shanmugaraj711-dotcom <shanmugaraj711@gmail.com>
2026-08-30 14:32:25 +0530

    debug: log CartLift analytics consent state

--- extensions/cartlift-web-pixel/src/index.ts
+++ extensions/cartlift-web-pixel/src/index.ts
@@ -25,6 +25,7 @@ register(({ analytics, init, customerPrivacy, settings }) => {
   if (!shop) return;
 
   let analyticsAllowed = init.customerPrivacy?.analyticsProcessingAllowed ?? true;
+  console.log("[CartLift DEBUG] analyticsAllowed:", analyticsAllowed, "raw:", init.customerPrivacy?.analyticsProcessingAllowed);
   customerPrivacy.subscribe("visitorConsentCollected", (event) => {
     analyticsAllowed = event.customerPrivacy.analyticsProcessingAllowed;
   });
